54c8bf54067c199099bf1c271aa2187d885b3767
Nathan <mangoldn0@gmail.com>
2025-03-11 23:16:29 -0600

    Added login.tsx and login route. Added functionality to login button

--- Lesso/app/components/login-button.tsx
+++ Lesso/app/components/login-button.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import '../app.css';
 
 const LoginButton: React.FC = () => {
+    const handleLoginClick = () => {
+        window.location.href = '/login';
+    };
+
     return (
         <div>
-            <button className="loginbutton" style={{ fontFamily: 'Orelega One, cursive' }}>
+            <button className="loginbutton" style={{ fontFamily: 'Orelega One, cursive' }} onClick={handleLoginClick}>
                 Login
             </button>
         </div>
