7fe3d4c8e403883da9404ee32e740327c48497b4
Nathan <mangoldn0@gmail.com>
2025-03-14 14:27:21 -0600

    Get started button redirects to plan page

--- Lesso/app/components/side-boarder.tsx
+++ Lesso/app/components/side-boarder.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Logo from "./logo";
 
 const SideBoarder: React.FC = () => {
+    const handleGetStarted = () => {
+        window.location.href = '/plan';
+    };
+
     return (
         <div className="fixed left-0 top-0 h-full w-full sm:w-1/3 flex flex-col items-center p-4 sm:p-6 shadow-xl text-white" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
             <div className="flex flex-col sm:flex-row items-center mt-4">
                 <h1 className="hidden sm:block text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold sm:mr-4">Lesso</h1>
                 <Logo className="w-24 h-24 sm:w-24 sm:h-24" />
             </div>
-            <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-xl text-center">Smarter & Faster Scheduling with AI-Powered Prompts.</p>
-            <button className="mt-6 sm:mt-10 px-4 py-2 sm:px-6 sm:py-3 bg-white text-[#44264E] font-semibold rounded-lg shadow-md hover:bg-gray-200">Get Started</button>
+            <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-3xl text-center">Smarter & Faster Scheduling with AI-Powered Prompts.</p>
+            <button onClick={handleGetStarted} className="mt-6 sm:mt-10 px-4 py-2 sm:px-6 sm:py-3 bg-white text-[#44264E] font-semibold rounded-lg shadow-md hover:bg-gray-200">Get Started</button>
             <style>{`
                 @media (max-width: 640px) {
                     div {
